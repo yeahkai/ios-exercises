4d7be5b6388f2b4e95a53bf22f9a8c73040f28b9
kai <yip.woo@gmail.com>
2016-09-12 20:20:51 -0400

    change what marry can purchase if the has x amount of dollars.

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -14,6 +14,20 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
+    if (dollars >= 1000000000) {
+        itemToReturn = @"have The Big Apple";
+    } else if (dollars >= 1000) {
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars >= 6) {
+        itemToReturn = @"have an apple";
+    }
+    else if (dollars >= 5){
+        itemToReturn = @"have some gum";
+    }
+    else if (dollars <=4) {
+        itemToReturn = @"get out of my store";
+    }
+    
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
