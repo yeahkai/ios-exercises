4b69dbd561ff48ba2be76857fdf68f00dc542e88
kai <yip.woo@gmail.com>
2016-09-15 14:43:54 -0400

    test that string work, ascending, descending, and equals.

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -10,9 +10,30 @@
 
 @implementation AwesomeCounter
 
-- (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+- (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber
+{ /* WORK HERE */
+    
+    if (number == otherNumber) {
+        return [NSString stringWithFormat:@"%ld", (long)number];
+    }
+    NSInteger lowNumber, highNumber;
+    
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    NSMutableString *returnString = [NSMutableString string];
+    
+    for (NSInteger i = lowNumber; i <= highNumber; i++) {
+        [returnString appendFormat:@"%ld", (long)i];
+    }
+    
+    return returnString;
 }
 
-@end
+    @end
+
