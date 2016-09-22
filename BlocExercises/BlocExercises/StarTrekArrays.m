a418d65879471a82f3b41b88b11a7ae3987a0e33
kai <yip.woo@gmail.com>
2016-09-22 14:52:55 -0400

    seperate character array with semicolon, alphabetized characters, locate character name containing worf.

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,34 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
+   return [characterString componentsSeparatedByString:@";"];
+                                                   
     return @[];
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
+    return [characterArray componentsJoinedByString:@";"];
     return @"";
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
+    NSArray *alphabetizedArray = [characterArray sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+         return alphabetizedArray;
     return @[];
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    for (NSString *characterName in characterArray) {
+        NSString *lowercaseName = characterName.lowercaseString;
+        NSRange WorfRange = [lowercaseName rangeOfString:@"worf"];
+        
+        if (WorfRange.location != NSNotFound) {
+return YES;
+        }
+}
     return NO;
 }
-
 @end
