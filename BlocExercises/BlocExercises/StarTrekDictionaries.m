8053baefe6ef5220acb9448d868056c97aa2273d
kai <yip.woo@gmail.com>
2016-09-23 10:30:16 -0400

    return favorite drink for startrek character, array of favorite drinks for startrek characters, added qoute to startrek character.

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,16 +12,31 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
+    return characterDictionary[@"favorite drink"];
+    
     return @"";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
+    NSMutableArray *array = [NSMutableArray array];
+    
+    for (NSDictionary *characterDictionary in charactersArray) {
+        [array addObject:characterDictionary[@"favorite drink"]];
+    }
+    
+    return array;
+
+    
     /* WORK HERE */
     return @[];
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
+    NSMutableDictionary *mutableDictionary = [characterDictionary mutableCopy];
+    mutableDictionary[@"quote"] = @"Today is a good day to die.";
+    return mutableDictionary;
+    
     return @{};
 }
 
